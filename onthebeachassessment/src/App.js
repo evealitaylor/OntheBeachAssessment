304769cd153855779db3d808e65e5c36bf836f9f
evealitaylor <159990522+evealitaylor@users.noreply.github.com>
2024-04-05 11:27:30 +0200

    star rating logic + further styling

--- onthebeachassessment/src/App.js
+++ onthebeachassessment/src/App.js
@@ -6,7 +6,7 @@ import Background from './Images/background.png'
 
 function App() {
   return (
-    <div className="App" style={{backgroundImage: `url(${Background})`, width:"100%", height:"100vh"}}>
+    <div className="App" style={{backgroundImage: `url(${Background})`, width:"100%"}}>
       <Hotel/>
     </div>
   );
--- onthebeachassessment/src/Hotels/Hotel.js
+++ onthebeachassessment/src/Hotels/Hotel.js
@@ -8,14 +8,11 @@ import SortPriceIcon from '../Images/SortPriceIcon.svg'
 import SortPriceSelectedIcon from '../Images/SortPriceSelectedIcon.svg'
 import SortRatingIcon from '../Images/SortRatingIcon.svg'
 import SortRatingSelectedIcon from '../Images/SortRatingSelectedIcon.svg'
-
-import HotelImage1 from '../Images/hotel-image-1.png';
-import HotelImage2 from '../Images/hotel-image-2.png';
-import HotelImage3 from '../Images/hotel-image-3.png';
+import StarRatingIcon from '../Images/StarRatingIcon.svg'
 
 export default function Hotel() {
 
-    const {item, setItem} = useState("priceSort");
+    const {sortHotelItem, setSortHotelItem} = useState("priceSort");
 
     // put a use reducer here for these
     const {alphabeticalSelect, setAlphabeticalSelect} = useState(false);
@@ -29,21 +26,21 @@ export default function Hotel() {
             name: "Iberostar Grand Salome",
             location: "Costa Adeje, Tenerife",
             image: "hotel-image-1",
-            stars: 5,
+            rating: 5,
             occupancy_adults: 2,
             occupancy_children: 2,
             occupancy_infants: 1,
             date: "3rd July 2019",
             duration: "7 days",
             departure: "East Midlands",
-            price: "1136.50",
+            price: "1,136.50",
             overview: "Located on the coast of Tenerife, between Playa del Duque and Playa de Fanabe, this hotel offers gourmet dining, exclusive lavish spas, magnificent suites with spectacular views and a personal butler or concierge service to meet all of your needs."
         },
         {
             name: "Aguamarina Golf Hotel",
             location: "Costa Adeje, Tenerife",
             image: "hotel-image-2",
-            stars: 4,
+            rating: 4,
             occupancy_adults: 2,
             occupancy_children: 1,
             occupancy_infants: 0,
@@ -57,7 +54,7 @@ export default function Hotel() {
             name: "Las Piramides Resort",
             location: "Costa Adeje, Tenerife",
             image: "hotel-image-3",
-            stars: 3,
+            rating: 3,
             occupancy_adults: 2,
             occupancy_children: 2,
             occupancy_infants: 0,
@@ -69,71 +66,87 @@ export default function Hotel() {
         }
     ]
 
-    // const sortHotels = (item) => {
-    //     if (item == "alphabeticalSort")
+    const sortHotels = (event) => {
+        if (sortHotelItem == "alphabeticalSort") {
         
-    //     setAlphabeticalSelect(true)
+        setAlphabeticalSelect(true)
+        setPriceSelect(false)
+        setRatingSelect(false)
 
-    //     hotelList.sort(function(a, b){return a.name - b.name})
+        hotelList.sort(function(a, b){return a.name - b.name})
 
-    //     else if (item == "priceSort")
+        } else if (sortHotelItem == "priceSort") {
 
-    //     setPriceSelect(true)
+        setPriceSelect(true)
+        setAlphabeticalSelect(false)
+        setRatingSelect(false)
 
-    //     hotelList.sort(function(a, b){return a.price - b.price})
+        hotelList.sort(function(a, b){return a.price - b.price})
 
-    //     else if (item == "ratingSort")
+        } else if (sortHotelItem == "ratingSort") {
 
-    //     setRatingSelect(true)
+        setRatingSelect(true)
+        setAlphabeticalSelect(false)
+        setPriceSelect(false)
 
-    //     hotelList.sort(function(a, b){return a.rating - b.rating})
-    // }
+        hotelList.sort(function(a, b){return a.rating - b.rating})
+        
+        }
+    }
 
   return (
-    <div>
-        <div className="sort-buttons-container">
-            {/* sort stuff */}
-            <div>
-                <button className="sort-button" onClick={()=>setItem("alphabeticalSort")}>
-                <p>sort alphabetically</p>
+    <div className='content-container'>
+        <div>
+            <div 
+                className={alphabeticalSelect ? "sort-button-selected" : "sort-button"}
+                onClick={(event)=> {
+                    setSortHotelItem("alphabeticalSort")
+                    sortHotels(event)
+                }}>
+                <p>sort <b>alphabetically</b></p>
                 <img src={alphabeticalSelect ? SortAlphabeticalSelectedIcon : SortAlphabeticalIcon} alt="Sort Alphabetical Icon"></img>
-                </button>
             </div>
-            <div>
-                <button className="sort-button" onClick={()=>setItem("priceSort")}>
-                <p>sort by price</p>
+            <div 
+                className={priceSelect ? "sort-button-selected" : "sort-button"}
+                onClick={(event)=> {
+                    setSortHotelItem("priceSort")
+                    sortHotels(event)
+                }}>
+                <p>sort by <b>price</b></p>
                 <img src={priceSelect ? SortPriceSelectedIcon : SortPriceIcon} alt="Sort Price Icon"></img>
-                </button>
             </div>
-            <div>
-                <button className="sort-button" onClick={()=>setItem("ratingSort")}>
-                <p>sort by star rating</p>
+            <div 
+                className={ratingSelect ? "sort-button-selected" : "sort-button"} 
+                onClick={(event)=> {
+                    setSortHotelItem("ratingSort")
+                    sortHotels(event)
+                }}>
+                <p>sort by <b>star rating</b></p>
                 <img src={ratingSelect ? SortRatingSelectedIcon : SortRatingIcon} alt="Sort Rating Icon"></img>
-                </button>
             </div>
         </div>
-        <div className='hotels-list-container'>
+        <div>
                 {hotelList.map((item, index) => (
-                    <div key={index}>
-                        <img src={require("../Images/" + item.image + ".png")} alt={item.name}></img>
-                        <div className='hotel-item-name'>{item.name}</div>
-                        <div className='hotel-item-location'>{item.location}</div>
-                        <div className='hotel-item-occupancy'><b>{item.occupancy_adults}</b> Adults, <b>{item.occupancy_children}</b> children</div>
-                        <div className='hotel-item-date-duration'><b>{item.date}</b> for <b>{item.duration}</b></div>
-                        <div className='hotel-item-date-duration'>departing from <b>{item.departure}</b></div>
-                        <div className='hotel-item-price'>Book Now {item.price}</div>
-                        <div>
-                            <button onClick={!setOverviewOpen}></button>
-
-                            {overviewOpen ?
-
-                            <div>{item.overview}</div>
-
-                            : null}
+                    <div key={index} className='hotel-item-container'>
+                        <img className='hotel-item-image' src={require("../Images/" + item.image + ".png")} alt={item.name}></img>
+                        <div className='hotel-item-content'>
+                            <div className='hotel-item-name'>{item.name}</div>
+                            <div className='hotel-item-location'>{item.location}</div>
+                            <div className='hotel-item-rating'>
+                                <img src={require("../Images/StarRatingIcon" + item.rating + ".png")}></img>
+                            </div>
+                            <div className='hotel-item-occupancy'><b>{item.occupancy_adults}</b> Adults, <b>{item.occupancy_children}</b> children & <b>{item.occupancy_infants}</b> infants</div>
+                            <div className='hotel-item-date-duration'><b>{item.date}</b> for <b>{item.duration}</b></div>
+                            <div className='hotel-item-departure'>departing from <b>{item.departure}</b></div>
+                            <div className='hotel-item-price-box'>
+                                <div>Book Now</div>
+                                <div className='hotel-item-price'>&pound;{item.price}</div>
+                            </div>
                         </div>
                     </div>
                 ))}
         </div>
+        <div></div>
     </div>
   )
 }
